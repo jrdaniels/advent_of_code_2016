bcab9749fe77b415b79b1be919e9f2a0bc9c5d5d
Jason Daniels <jason@jasondaniels.net>
2016-12-13 21:23:49 -0600

    Day 6
    This was pretty easy.  Used some of the code from before

---
+++ day06/signal-and-noise.exs
@@ -0,0 +1,31 @@
+defmodule Message do
+  def parse(input) do
+    input
+    |> rotate
+    |> Enum.map(&letter_count/1)
+    |> Enum.join 
+  end
+
+  def rotate(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.reduce([],fn line, acc -> [String.graphemes(line) | acc] end)
+    |> List.zip
+  end
+
+  def letter_count(line) do
+    line
+    |> Tuple.to_list
+    |> Enum.reduce(Map.new, fn(letter, map) -> Map.update(map, letter, 1, &(&1 + 1)) end)
+    |> Enum.sort_by(fn({_k, v}) -> v end, &>=/2 )
+    |> Enum.take(1)
+    |> Enum.map(fn {letter, _} -> letter end)
+  end
+
+end
+
+System.argv
+  |> hd
+  |> File.read!
+  |> Message.parse
+  |> IO.inspect

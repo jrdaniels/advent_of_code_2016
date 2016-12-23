f877c9da3a68597e5ecbb76082491053fd72eb86
Jason Daniels <jrdaniels@containerstore.com>
2016-12-23 12:32:24 -0600

    Finally Solved part 1

--- day07/ip7.exs
+++ day07/ip7.exs
@@ -2,13 +2,28 @@ defmodule IP7 do
   def validate(input) do
     input
     |> get_addresses
-    |> Enum.filter(fn line -> !String.match?(line, ~r/\[.*?[^\]]*?(.)(?!\1)(.)\2\1.*?\]/) end)
-    |> Enum.filter(fn line -> String.match?(line, ~r/(.)(?!\1)(.)\2\1/ ) end)
+    |> Enum.filter(&filter_abba_in_hypernet(&1))
+    |> Enum.filter(&contains_abba?/1)
+    # |> Enum.filter(fn line -> String.match?(line, ~r/\[.*?[^\]]*?(.)(?!\1)(.)\2\1.*?\]/) end)
+    # |> Enum.map(&Regex.replace(~r/\[.*?[^\]]*?(.)(?!\1)(.)\2\1.*?\]/, &1, "**\\0**"))
+    # |> Enum.filter(fn line -> String.match?(line, ~r/(.)(?!\1)(.)\2\1/ ) end)
   end
 
   def get_addresses(input) do
     input
     |> String.split("\n", trim: true)
+  end
+
+  def filter_abba_in_hypernet(address) do
+    address
+    |> String.split(["[","]"])
+    |> Enum.drop_every(2)
+    |> Enum.filter(&contains_abba?/1)
+    |> Enum.empty?
+  end
+
+  def contains_abba?(string) do
+    String.match?(string, ~r/(.)(?!\1)(.)\2\1/ )
   end
 
 end
